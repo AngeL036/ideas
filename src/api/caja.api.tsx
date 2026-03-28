fd26158ff8a59965f490f2b73ff7b0a60f1e1e16
Angel Sanchez <angelgabriel036@gmail.com>
2026-03-28 16:04:12 -0600

    llamados al api

--- src/api/caja.api.tsx
+++ src/api/caja.api.tsx
@@ -1,11 +1,11 @@
 import api from './api'
 
 export const getCajaActiva = async () => {
-    const res = await api.get("/cajaActiva/")
+    const res = await api.get("/caja/cajaActiva")
     return res.data
 }
 
 export const abrirCaja = async (monto:number) => {
-    const res = await api.post("/abrirCaja/", monto)
+    const res = await api.post("/caja/abrirCaja", { monto_inicial: monto})
     return res.data
 }
